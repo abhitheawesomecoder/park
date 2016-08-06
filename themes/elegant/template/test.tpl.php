0e91c678f3eef9272db05602140c84f987d4586d
Abhishek Kumar <abhitheawesomecoder@gmail.com>
2016-08-06 21:20:50 +0530

    Posts done

--- themes/elegant/template/test.tpl.php
+++ themes/elegant/template/test.tpl.php
@@ -94,3 +94,82 @@ if($media['type'] == 'vid') {
 </div>
 <? }?>
 <? } ?> <!-- OVERVIEW -->
+
+
+<? if($_GET['subaction'] == 'posts') { ?> <!-- POSTS -->
+
+	<div id="container" style="margin-left: 3px; margin-top: 5px;">
+		<? $user_news = get_profile_user_news($_GET['profile']); ?>
+
+		<? $i = 7;
+		   foreach(array_slice($user_news, $startResults, $resultsPerPage) as $media):
+		   $query_user = mysql_query("SELECT * FROM users WHERE id='".$media['author']."'");
+			 while($user_media = mysql_fetch_array($query_user)) {
+				 $first_name = $user_media['first_name'];
+				 $last_name = $user_media['last_name'];
+			 }
+		?>
+
+		<div class="box">
+		<a href="<?=$root;?><? $cat_query = mysql_query("SELECT * FROM categories WHERE id=".$media['cat']."");
+		$cat_row = mysql_fetch_row($cat_query);
+		if($cat_row['2'] == '') {
+			$cat_row = 'other';
+		}else{
+			$cat_row=$cat_row['2'];
+		}
+		if($SETTINGS['permalink'] == 'gag') {
+			echo "/gag/".$media['news_id'];
+		} elseif($SETTINGS['permalink'] == 'cat') {
+			echo "/".$cat_row."/".$media['news_id'];
+		} elseif($SETTINGS['permalink'] == 'cat_slugify') {
+			echo "/".$cat_row."/".slugify($media['title']);
+		} ?>">
+		<div class="box-media-title"><?=$media['title']?></div>
+		<? $cat_display = mysql_query("SELECT * FROM categories WHERE id=".$media['cat']."");
+		   $cat_display_row = mysql_fetch_row($cat_display);
+		?>
+		<?
+		if($media['type'] == 'pic') {
+			echo '<div class="gif-post"><img src='.$root."/uploads/media_photos/thumb_".$media['file'].' width="'.$box_type.'" alt="'.$media['title'].'" title="'.$media['title'].'"></a></div>';
+		}
+		if($media['type'] == 'gif') {
+			echo '</a><div class="gif-post"><img class="gif-image animation" id='.$media['news_id'].' src='.$root."/uploads/media_photos/".$media['thumb'].' data-animation='.$root."/uploads/media_photos/".$media['file'].' data-original='.$root."/uploads/media_photos/".$media['thumb'].' data-state="0" width="'.$box_type.'" /> <span class="play">GIF</span></div>';
+		}
+		if($media['type'] == 'vid') {
+			echo '<div class="gif-post"><img src='.$root."/uploads/media_photos/".$media['file'].' width="'.$box_type.'"><span class="play_video">VIDEO</span></a></div>';
+		}
+		?>
+		<? if(SHARE_SOCIAL_NETWORK_TURN == '1') { ?>
+		<div class="share">
+		<ul>
+		    <li><a class="btn-share facebook" onclick="javascript:fbshare('<?=$root;?><? $cat_query = mysql_query("SELECT * FROM categories WHERE id=".$media['cat'].""); $cat_row = mysql_fetch_row($cat_query); if($cat_row['2'] == '') { $cat_row = 'other'; }else{ $cat_row=$cat_row['2']; } if($SETTINGS['permalink'] == 'gag') { echo "/gag/".$media['news_id']; } elseif($SETTINGS['permalink'] == 'cat') { echo "/".$cat_row."/".$media['news_id']; } elseif($SETTINGS['permalink'] == 'cat_slugify') { echo "/".$cat_row."/".slugify($media['title']); } ?>')">Facebook</a></li>
+		    <li><a class="btn-share twitter" onclick="javascript:twittershare('<?=$media['title']?>','<?=$root;?><? $cat_query = mysql_query("SELECT * FROM categories WHERE id=".$media['cat'].""); $cat_row = mysql_fetch_row($cat_query); if($cat_row['2'] == '') { $cat_row = 'other'; }else{ $cat_row=$cat_row['2']; } if($SETTINGS['permalink'] == 'gag') { echo "/gag/".$media['news_id']; } elseif($SETTINGS['permalink'] == 'cat') { echo "/".$cat_row."/".$media['news_id']; } elseif($SETTINGS['permalink'] == 'cat_slugify') { echo "/".$cat_row."/".slugify($media['title']); } ?>')" data_title="<?=$media['title']?>">Twitter</a></li>
+		    <li><a class="btn-share google" onclick="javascript:googleshare('<?=$root;?><? $cat_query = mysql_query("SELECT * FROM categories WHERE id=".$media['cat'].""); $cat_row = mysql_fetch_row($cat_query); if($cat_row['2'] == '') { $cat_row = 'other'; }else{ $cat_row=$cat_row['2']; } if($SETTINGS['permalink'] == 'gag') { echo "/gag/".$media['news_id']; } elseif($SETTINGS['permalink'] == 'cat') { echo "/".$cat_row."/".$media['news_id']; } elseif($SETTINGS['permalink'] == 'cat_slugify') { echo "/".$cat_row."/".slugify($media['title']); } ?>')" data_title="<?=$media['title']?>">Google</a></li>
+		</ul>
+		</div>
+		<? } ?>
+
+	</div>
+
+	<? $i++;
+	endforeach; ?>
+</div>
+
+<!-- contains the content to be loaded when scrolled -->
+<nav id="page-nav">
+<? if($_GET['page'] = '') { $page=$_GET['page']; } else { $page='1'; } ?>
+  <a href="<?=$root;?>/?view=account&action=profile&subaction=posts&profile=<?=$_GET['profile']?>&page=<?=$page+1;?>"></a>
+</nav>
+
+<?
+$result_posts = mysql_query("SELECT n.id,n.news_id,n.title,n.type,n.thumb,n.file,n.cat,n.status,n.author FROM news n WHERE n.author='".$Users['0']."' ORDER BY id DESC");
+
+if (mysql_num_rows($result_posts)==0) {?>
+<div class="blank-state">
+        <h3><?=$LANG['posts_show_up'];?> <?=$Users['6']?> <?=$Users['7']?> <?=$LANG['uploaded_will_show_up_here'];?></h3>
+        <div class="btn-container" style="text-align: center; width: auto;"><a class="btn" href="<?=$root?>/view/popular"><?=$LANG['checkout_whats_hot'];?></a></div>
+</div>
+<? }?>
+
+<? } ?> <!-- POSTS -->
